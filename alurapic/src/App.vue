bc37c5b9921b47000e84cdbdf80583df502bd840
vhavhugo <vhavhugo@hotmail.com>
2021-05-04 22:21:17 -0300

    O poder das diretivas

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,7 +1,11 @@
 <template>
 <div>
   <h1>{{ titulo }}</h1>
-  <img :src="foto.url" :alt="foto.titulo"/>
+  <ul>
+    <li v-for="foto of fotos">
+      <img :src="foto.url" :alt="foto.titulo"/>
+    </li>
+  </ul>
 </div>
 </template>
 
@@ -10,10 +14,15 @@ export default {
   data(){
     return{
       titulo: 'Alurapic',
-      foto: {
-        url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.F4C7GmtZcmeDikxFHIx02QHaFj%26pid%3DApi%26h%3D160&f=1',
-        titulo: "cachorro"
-      }
+      fotos: [
+        {
+          url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.F4C7GmtZcmeDikxFHIx02QHaFj%26pid%3DApi%26h%3D160&f=1',
+          titulo: "cachorro"
+        },
+        {
+        url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Jf0NnGpH2AhNM3BtwZufwwHaJ4%26pid%3DApi%26h%3D160&f=1',
+        titulo: "cachorrão"
+      }]
     }
   }
 }
