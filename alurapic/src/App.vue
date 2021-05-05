93feb9b7520b8fd0e726804c31e22d00c93f272a
vhavhugo <vhavhugo@hotmail.com>
2021-05-05 20:11:01 -0300

    Filtando lista

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -3,6 +3,8 @@
 <div class="corpo">
   <h1 class="centralizado">{{ titulo }}</h1>
 
+  <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="filtre por parte do título">
+  {{ filtro }}
   <ul class="lista-fotos">
     <li class="lista-fotos-item" v-for="foto of fotos">
       
@@ -26,7 +28,8 @@ export default {
   data(){
     return{
       titulo: 'Alurapic',
-      fotos: []
+      fotos: [],
+      filtro: ''
       
     }
   },
@@ -65,30 +68,9 @@ export default {
     width: 100%;
   }
 
-  /* estilo do painel */ 
-
-   .painel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
+  .filtro{
+    display:block;
+    width: 100%;
   }
 
-  .painel .painel-titulo {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 15px 0;
-    padding: 10px;
-    text-transform: uppercase;
-  }
-
-  .painel *{
-    box-shadow: 5px 5px 10px black;
-  }
 </style>
--- alurapic/src/components/shared/painel/Painel.vue
+++ alurapic/src/components/shared/painel/Painel.vue
@@ -15,7 +15,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
    .painel {
     padding: 0 auto;
     border: solid 2px grey;
