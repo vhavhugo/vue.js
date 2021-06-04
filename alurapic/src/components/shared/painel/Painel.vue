9abdd5faa69446c9abde39a34a3d6a1a6fcd48f0
vhavhugo <vhavhugo@hotmail.com>
2021-06-04 08:26:31 -0300

    Estilos e escopos

--- alurapic/src/components/shared/painel/Painel.vue
+++ alurapic/src/components/shared/painel/Painel.vue
@@ -11,7 +11,8 @@ export default {
  }
 </script>
 
-<style>
+<!-- scoped = serve para encapsular o css no component -->
+<style scoped>
 
     /* estilo do painel */ 
 
@@ -34,5 +35,9 @@ export default {
     margin: 0 0 15px 0;
     padding: 10px;
     text-transform: uppercase;
+  }
+
+  * {
+    box-shadow: 5px 5px 5px;
   }
 </style>
